1d3c327615ad002f49947634388e8a27c6b9551e
Satrio215 <satrio.ajinoto@gmail.com>
2025-03-17 23:05:33 +0700

    Navbar Admin

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -30,6 +30,12 @@ export default function AuthenticatedLayout({ header, children }) {
                                 >
                                     Dashboard
                                 </NavLink>
+                                <NavLink
+                                    href={route('forms.index')}
+                                    active={route().current('forms.index')}
+                                >
+                                    Pengaduan
+                                </NavLink>
                             </div>
                         </div>
 
@@ -133,6 +139,12 @@ export default function AuthenticatedLayout({ header, children }) {
                             active={route().current('dashboard')}
                         >
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            href={route('forms.index')}
+                            active={route().current('forms.index')}
+                        >
+                            Pengaduan
                         </ResponsiveNavLink>
                     </div>
 
